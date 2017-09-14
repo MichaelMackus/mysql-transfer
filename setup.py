544bc1feac73b427703266924d338fd513433def
Michael Mackus <michaelmackus@gmail.com>
2017-09-14 12:51:58 -0700

    Add PIP setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mysql-transfer',
+      version='0.1',
+      description='Transfer MySQL databases. Also has support for '
+                  'loading parameters from YAML files and checking '
+                  'for a migration version match (before transfer).',
+      url='http://github.com/MichaelMackus/mysql-transfer',
+      author='Michael Mackus',
+      author_email='michaelmackus@gmail.com',
+      license='MIT',
+      packages=['mysql_transfer'],
+      zip_safe=False)
